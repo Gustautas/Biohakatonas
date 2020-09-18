fe58f12b879dafde3b400396ec4384555f53f66e
Ben <benas.kaz@gmail.com>
2020-09-19 01:48:20 +0300

    Augmented shitton of data

---
+++ images/augment_flip.py
@@ -0,0 +1,63 @@
+import imgaug.augmenters as iaa
+import cv2
+import xml.etree.ElementTree as ET
+import os
+
+images_list = []
+height = 512
+width = 512
+
+for file in os.listdir("labeled_real_data_non_augmented_tik_simo"):
+    if file.endswith(".jpg"):
+        images_list.append(file[:-4])
+
+
+for image in images_list:
+    filename = image
+    filename_with_extension = filename + ".xml"
+
+
+    label_org_xml = ET.parse("labeled_real_data_non_augmented_tik_simo/" + filename_with_extension)
+
+    annotation = label_org_xml.getroot()
+
+    for child in annotation.findall("object"):
+        bndbox = child.find("bndbox")
+
+        xmin = bndbox.find("xmin")
+        xmin_text_old = xmin.text
+
+        xmax = bndbox.find("xmax")
+        xmax_text_old = xmax.text
+
+        ymin = bndbox.find("ymin")
+        ymin_text_old = ymin.text
+
+        ymax = bndbox.find("ymax")
+        ymax_text_old = ymax.text
+
+
+        xmin.text = str(width - int(xmax_text_old))
+
+        xmax.text = str(width - int(xmin_text_old))
+
+        ymin.text = ymin_text_old
+
+        ymax.text = ymax_text_old
+
+
+    label_org_xml.write("labeled_real_data_augmented_tik_simo/" + filename + "flipped.xml")
+
+    seq = iaa.Sequential([
+        iaa.Fliplr(1),
+    ])
+
+    imglist = []
+
+    img = cv2.imread("labeled_real_data_non_augmented_tik_simo/" + filename + ".jpg")
+
+    imglist.append(img)
+
+    images_aug = seq.augment_images(imglist)
+
+    cv2.imwrite("labeled_real_data_augmented_tik_simo/" + filename + "flipped.jpg", images_aug[0])
---
+++ images/augment_rotate.py
@@ -0,0 +1,74 @@
+import imgaug.augmenters as iaa
+import cv2
+import xml.etree.ElementTree as ET
+import math
+import numpy as np
+import os
+
+rotation_degrees = 270
+rotation_degrees_rad = rotation_degrees / 180 * math.pi
+height = 512
+width = 512
+
+images_list = []
+
+for file in os.listdir("labeled_real_data_non_augmented_tik_simo"):
+    if file.endswith(".jpg"):
+        images_list.append(file[:-4])
+
+for image in images_list:
+    filename = image
+    filename_with_extension = filename + ".xml"
+
+    label_org_xml = ET.parse("labeled_real_data_non_augmented_tik_simo/" + filename_with_extension)
+
+    annotation = label_org_xml.getroot()
+
+    for child in annotation.findall("object"):
+        bndbox = child.find("bndbox")
+
+        xmin = bndbox.find("xmin")
+        xmin_text_old = xmin.text
+
+        xmax = bndbox.find("xmax")
+        xmax_text_old = xmax.text
+
+        ymin = bndbox.find("ymin")
+        ymin_text_old = ymin.text
+
+        ymax = bndbox.find("ymax")
+        ymax_text_old = ymax.text
+
+
+        xy = np.array([int(xmin_text_old) - width / 2,int(ymin_text_old) - height / 2])
+        c ,s = np.cos(rotation_degrees_rad), np.sin(rotation_degrees_rad)
+        R = np.array(((c, -s), (s, c)))
+        rot = np.dot(R,xy)
+
+        xmin.text = str(int(rot[0]) + width / 2)
+        ymin.text = str(int(rot[1]) + height / 2)
+
+        xy = np.array([int(xmax_text_old) - width / 2, int(ymax_text_old) - height / 2])
+        c, s = np.cos(rotation_degrees_rad), np.sin(rotation_degrees_rad)
+        R = np.array(((c, -s), (s, c)))
+        rot = np.dot(R, xy)
+
+        xmax.text = str(int(rot[0]) + width / 2)
+        ymax.text = str(int(rot[1]) + height / 2)
+
+
+    label_org_xml.write("labeled_real_data_augmented_tik_simo/" + filename + str(rotation_degrees) + ".xml")
+
+    seq = iaa.Sequential([
+        iaa.Rotate(rotation_degrees),
+    ])
+
+    imglist = []
+
+    img = cv2.imread("labeled_real_data_non_augmented_tik_simo/" + filename + ".jpg")
+
+    imglist.append(img)
+
+    images_aug = seq.augment_images(imglist)
+
+    cv2.imwrite("labeled_real_data_augmented_tik_simo/" + filename + str(rotation_degrees) + ".jpg", images_aug[0])
